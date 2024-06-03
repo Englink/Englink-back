c80bd757b1ed1d5f64eee746b3fbae226d39f801
shlomohalevi <shlomoww@gmail.com>
2024-06-03 12:43:13 +0300

    retrive also image with review

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -5,103 +5,90 @@ const availability = require('../models/availability')
 const user = require('../models/usersModel')
 const reviews = require('../models/reviews')
 
-
-
 exports.getAllteachers = asyncHandler(async (req, res, next)=>{
    
     const teachers = await user.find({role:'teacher'})
-
     res.status(200).json({
         status:'success',
         teachers
 })
 })
-    
-   
-    
+
 exports.updateTeacherAvailability = asyncHandler(async (req, res, next)=>{
-    // console.log(moment.tz.guess())
+
     const tcId =  req.user._id
-    // console.log(tcId)
     const {month, year,day,hour,minute} = req.body.date;
-    // const availibleDate = moment.tz(new Date(year, month-1,day,hour,minute,0), "Asia/Jerusalem");
-
     const availibleDate = new Date(year, month-1,day,hour,minute,0);
-    // console.log(new Date(availibleDate).toLocaleTimeString())
-    // return next(new AppError(500, 'cannot set availability in this date ,becouse lesson'))
-
     const teacherDates = await availability.find({teacherId:tcId})
+
     if (teacherDates.some(dObj=>
         {
-            return  Math.abs(dObj.date.getTime() - availibleDate.getTime()) / 60000 < 20;
+            return  Math.abs(dObj.date.getTime() - availibleDate.getTime()) / 60000 < 30;
         }))
         {
             return next(new AppError(500, 'cannot set availability in this date ,becouse already set availability in this time'))
         }
         const teacherLessons = await appointment.find({teacherId:tcId})
-        if (teacherLessons.some(dObj=>
-            {
-                return  Math.abs(dObj.date.getTime() - availibleDate.getTime()) / 60000 < 20;
-            }))
-            {
-                return next(new AppError(500, 'cannot set availability in this date ,becouse lesson'))
-            }
+    if (teacherLessons.some(dObj=>
+        {
+            return  Math.abs(dObj.date.getTime() - availibleDate.getTime()) / 60000 < 30;
+        }))
+        {
+            return next(new AppError(500, 'cannot set availability in this date ,becouse lesson'))
+        }
 
         
-        const newAvailibltiy = await availability.create({date:availibleDate,teacherId:tcId})
-        if (newAvailibltiy)
-            {
-                res.status(200).json({
-                    status: 'success',
-                    newAvailibltiy
-                    
-                });
-            }
+    const newAvailibltiy = await availability.create({date:availibleDate,teacherId:tcId})
+    if (newAvailibltiy)
+        {
+            res.status(200).json({
+                status: 'success',
+                newAvailibltiy
+                
+            });
+        }
         else
         return next(new AppError(500,'sumthing went wrong'))
 
     });
+    
+   
+    
+
      
     exports.getTeacherAvailability = asyncHandler(async (req, res, next)=>
         {
             const tcId = req.params.id; 
-
             const teacherAvailability = await availability.find({ teacherId: tcId })
             .populate({
               path: 'teacherId'})
+              
+              if(!teacherAvailability)
+                  return next(new AppError(500,'availability not found'))
+              res.status(200).json({
+                  status: 'success',
+                  teacherAvailability
+              });
+          })
              
                     
             
-            // console.log(teacherDates)
-            if(!teacherAvailability)
-                return next(new AppError(500,'availability not found'))
-            res.status(200).json({
-                status: 'success',
-                teacherAvailability
-            });
-        })
     
         exports.CanceleTeacherAvailability = asyncHandler(async (req, res, next)=>
             {
                 const cancelDates = req.body.dates
                 const tcId = req.user._id
-                const datesToRemove = cancelDates.map(date => new Date(date));
                 
-                console.log(datesToRemove)
-                // Remove all documents that match any of the dates in datesToRemove
-                const cancelledDate = await availability.deleteMany({      teacherId: tcId,
+                const datesToRemove = cancelDates.map(date => new Date(date));
+                const cancelledDate =  await availability.deleteMany({teacherId: tcId,
                     date: { $in: datesToRemove } });
-                    
-                    
                     res.status(200).json({
                         status: 'success',
                         cancelledDate                    
                     });
                 })
-                // const availabilityId =  req.params.id;
-            // const cancelledDate = await availability.findByIdAndDelete(availabilityId)
-            // if(!cancelledDate)
-            //     return next(new AppError(500,'date not found'))
+                    
+                    
         exports.GetTeacherLessons = asyncHandler(async (req, res, next)=>
             {
                 const tcId = req.user._id
@@ -164,14 +151,14 @@ exports.updateTeacherAvailability = asyncHandler(async (req, res, next)=>{
             const tcId = req.params.id; 
             const teacherReviews = await reviews.find({teacherId:tcId})
             .populate('teacherId', 'name')
-            .populate('studentId', 'name');
+            .populate('studentId', 'name image');
             res.status(200).json({
                 status: 'success',
                 teacherReviews: teacherReviews
             });
                 
-                
         })
+                
     
 
 

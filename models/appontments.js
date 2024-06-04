4a688cd7e0a96e1f933a2d7bce47da4efb483333
shlomohalevi <shlomoww@gmail.com>
2024-06-04 10:20:03 +0300

    notifications and forgot password update

--- models/appontments.js
+++ models/appontments.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 const schedule = require('node-schedule');
 
+const notificationSchema = new mongoose.Schema({
+    start: {
+        type: String, // Store the job ID for the start notification
+        default: null // Default value for start notification
+    },
+    end: {
+        type: String, // Store the job ID for the before notification
+        default: null // Default value for start notification
+    },
+    // Add more fields for other notification types as needed
+});
+
+
 const appointmentSchema = new mongoose.Schema({
     teacherId: {
         type: mongoose.Schema.Types.ObjectId,
@@ -16,9 +29,7 @@ const appointmentSchema = new mongoose.Schema({
         type: Date,
         required: true
     },
-    notificationJobId: {
-        type: String // Store the job ID returned by the scheduling library
-      }
+    notifications: notificationSchema,
     
 
 });
@@ -31,22 +42,41 @@ appointmentSchema.set('toJSON', {
         delete ret.teacherDetails.password;
         delete ret.studentDetails.password
     }
-       
     
-    // status: {
-    //     type: String,
-    //     enum: ['scheduled', 'canceled', 'completed'],
-    //     default: 'scheduled'
-    // },
-    // Add any other fields you need for the appointment
 });
-appointmentSchema.pre('deleteOne', async function(next) {
-    if (this.notificationJobId && schedule.scheduledJobs[this.notificationJobId])
-        schedule.scheduledJobs[Appointment.notificationJobId].cancel()
-next();
-  });
+
+appointmentSchema.pre('deleteOne',{ document: true, query: false }, async function(next) {
+    try {
+        const notifications = this.notifications;
+        
+        if (notifications.start && schedule.scheduledJobs[notifications.start]) {
+            schedule.scheduledJobs[notifications.start].cancel();
+        }
+
+        if (notifications.end && schedule.scheduledJobs[notifications.end]) {
+            schedule.scheduledJobs[notifications.end].cancel();
+        }
+
+        // Add more cancellation logic for other notification types if needed
+
+        next();
+    } catch (err) {
+        next(err);
+    }
+
+}
+);
 
 
 const Appointment = mongoose.model('Appointment', appointmentSchema);
 
 module.exports = Appointment;
+
+
+
+// status: {
+//     type: String,
+//     enum: ['scheduled', 'canceled', 'completed'],
+//     default: 'scheduled'
+// },
+// Add any other fields you need for the appointment

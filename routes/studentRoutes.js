e75efecba0a5d20dd82fe3a6f0c79c829d8110fb
shlomohalevi <shlomoww@gmail.com>
2024-06-10 09:01:12 +0300

    image update

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -6,7 +6,6 @@ const teacherControllers = require('./../controllers/teacherController')
 const router = express.Router()
 const upload = require('../upload')
 
-// router.route('/update-image-test').post(authControllers.protect,upload.single('image'),studentControllers.Update_the_user_information)
 router.route('/').get(authControllers.protect, studentControllers.getAllstudents)
 router.route('/updating-user-details').put(authControllers.protect,upload.single('image'),studentControllers.Update_the_user_information)
 router.route('/set-lesson/:id').put(authControllers.protect,authControllers.restrictTo(['student']), studentControllers.setLesson)
@@ -15,8 +14,13 @@ router.route('/login').post(authControllers.login)
 router.route('/cancele-lesson/:id').delete(authControllers.protect,studentControllers.CanceleLesson)
 router.route('/get-student-lessons').get(authControllers.protect,authControllers.restrictTo(['student']),studentControllers.GetStudentsLessons)
 router.route('/validate').get(authControllers.protect,authControllers.validUser)
-router.route('/test').post(studentControllers.test)
 
 router.route('/deletion').delete(authControllers.protect,studentControllers.DeleteStudent)
+// router.route('/test').post(studentControllers.test)
+router.route('/update-image-test').post(upload.array('files',10),(req,res)=>
+{
+    res.send('Files uploaded successfully');
+ 
+})
 
 module.exports = router

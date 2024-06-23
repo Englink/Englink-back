e404a707f13f71287f29b26e53272bf361a65a50
shlomohalevi <shlomoww@gmail.com>
2024-06-23 11:15:31 +0300

    Revert "backend update"
    This reverts commit 8be63cc0219ce7698d13a934923d8813a8276f4e.

--- utils/sending_messages.js
+++ utils/sending_messages.js
@@ -97,23 +97,9 @@ const sendEmailRegisration = async (email,role,name)=>
 
             await sendEmail({
                 to: email,
-                subject: '!ברוך הבא',
-                html: `
-                    <html>
-                    <head>
-                        <style>
-                            body {
-                                direction: rtl;
-                                text-align: right;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <p>!תודה על הרשמתך לאינגלינק ${name} ברוך הבא</p>
-                    </body>
-                    </html>
-                `
-                    
+                subject: 'Welcome to Our Website',
+                html: `<h1>Welcome ${role} ${name}</h1><p>Thank you for registering to Englink!</p>`
+                
               });   
             
         }
